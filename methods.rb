446084882e1e97c7123fec69183e4831f65e8863
hassan kazungu <hassankazungu01@gmail.com>
2023-02-07 07:30:27 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default (name = 'programmer')
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    return num1 + num2
+    sum = add(1, 2)
+    puts sum
+end
+
+# def halve (number)
+#     if number.class != "Integer"
+#         return nil
+#     end
+    
+#     return number/2
+# end
+
+def halve num
+    if num.class != Integer
+         return nil
+    end
+    return num / 2
+    end
+
